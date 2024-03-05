a43c64555108245f9444a73256aa5164ef64a283
rnitta <ryushironitta@yahoo.co.jp>
2024-03-05 18:05:02 +0900

    updater.shのbranch名に変数を使用

--- Server/VersionManager/updater.sh
+++ Server/VersionManager/updater.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-BRANCH_NAME = "test_VersionManager"
+BRANCH_NAME="test_checkupdate"
 
 # リモートの release ブランチの最新のハッシュを取得
-remote_commit=$(git ls-remote origin $(BRANCH_NAME) | awk '{print $1}')
+remote_commit=$(git ls-remote origin $BRANCH_NAME | awk '{print $1}')
 # ローカルの release ブランチの最新のハッシュを取得
-local_commit=$(git rev-parse $(BRANCH_NAME))
+local_commit=$(git rev-parse $BRANCH_NAME)
 
 # リモートとローカルのハッシュを比較
 if [ "$remote_commit" != "$local_commit" ]; then
@@ -13,16 +13,16 @@ if [ "$remote_commit" != "$local_commit" ]; then
 
     # リモートの README を表示
     echo "リモートの最新の README ファイルの内容は以下の通りです:"
-    git fetch origin $(BRANCH_NAME)
-    git show origin/$(BRANCH_NAME):README.md
+    git fetch origin $BRANCH_NAME
+    git show origin/$BRANCH_NAME:README.md
 
     # ユーザに更新するかどうかを問いかける
     read -p "ローカルの release ブランチを更新しますか？ (y/n): " answer
     case $answer in
         [Yy]* )
             # ブランチを更新
-            git checkout $(BRANCH_NAME)
-            git pull origin $(BRANCH_NAME)
+            git checkout $BRANCH_NAME
+            git pull origin $BRANCH_NAME
             echo "ローカルの release ブランチを更新しました。"
             ;;
         * )
